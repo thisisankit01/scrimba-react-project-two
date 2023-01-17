f511e2241ecb380c10166e14764687924ccb87c1
Ankit Pandey <ankitechzz@gmail.com>
2023-01-17 19:49:49 +0530

    module 2 complted

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,26 @@ import Hero from "./Hero"
 import Card from "./Card"
 import data from "./Data"
 import './index.css'
-import photo1 from './assets/photo1.png'
+
 
 export default function App() {
-
-  const cards = data.map((item)=>{
-    return(
-      <Card
-      img = {item.coverImg}
-      rating={item.stats.rating}
-      reviewCount={item.stats.reviewCount}
-      location={item.location}
-      title={item.title}
-      price={item.price}
-      />
-    )
-  })
-
-
-  return(
-   <div>
-    <Navbar/>
-    <Hero/>
-    <div className="flex flex-row flex-wrap">
-    {cards}
-    </div>
-   </div>
+  const cards = data.map(item => {
+      return (
+          <Card
+              key={item.id}
+              {...item}
+              
+          />
+      )
+  })        
+  
+  return (
+      <div>
+          <Navbar />
+          <Hero />
+          <section className="flex flex-row flex-wrap">
+              {cards}
+          </section>
+      </div>
   )
-
 }
--- src/Card.tsx
+++ src/Card.tsx
@@ -3,32 +3,35 @@
  import './index.css'
  import './output.css'
 
- export default function Card(props : any) {
+
+export default function Card(props : any) {
+  let badgeText
+  if (props.openSpots === 0) {
+      badgeText = "SOLD OUT"
+  } else if (props.location === "Online") {
+      badgeText = "ONLINE"
+  }
+  
   return (
-    <div className="card-container flex flex-row">
-    <div className="card flex flex-col">
-   <img src={`${photo1}`} alt="" className='card--image' />
-   <div className="card--stats">
-    <img src={star} className="card--star"/>
-      <span> {props.rating}</span>
-      <span className='gray'> {props.reviewCount}</span>
-      <span className='gray'>{props.country}</span>
-    </div>
-    <p className='space-line'>{props.title}</p>
-    <p className='space-line'><span className='bold'>From ${props.price} </span>/ person</p>
-    </div>
-    </div>
+      <div className="card">
+          {
+              badgeText && 
+              <div className="card--badge">{badgeText}</div>
+          }
+          <img 
+              src={photo1} 
+              className="card--image" 
+          />
+          <div className="card--stats">
+              <img src={star} className="card--star" />
+              <span>{props.stats.rating}</span>
+              <span className="gray">({props.stats.reviewCount}) • </span>
+              <span className="gray">{props.location}</span>
+          </div>
+          <p className="card--title">{props.title}</p>
+          <p className="card--price">
+              <span className="bold">From ${props.price}</span> / person
+          </p>
+      </div>
   )
- }
-
-/*
-Challenge: Pass props to the Card component and display that data
-
-- img ("katie-zaferes.png")
-- rating ("5.0")
-- reviewCount (6)
-- country (Whatever you want)
-- title ("Life Lessons with Katie Zaferes")
-- price (136)
-
-*/
+}
